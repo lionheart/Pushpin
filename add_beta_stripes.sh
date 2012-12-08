d82cedb4756d63381c8e6c352c7fd322657e2a61
Dan Loewenherz <dloewenherz@gmail.com>
2012-12-08 08:23:58 -0800

    add beta stripes build fails

--- add_beta_stripes.sh
+++ add_beta_stripes.sh
@@ -6,3 +6,10 @@ convert "Assets/Icon-settings.png" -draw "image over 0,0 29,29 'Assets/Beta-Over
 convert "Assets/Icon-settings@2x.png" -draw "image over 0,0 58,58 'Assets/Beta-Overlay.png'" "Assets/Icon-settings-beta@2x.png"
 convert "Assets/iTunesArtwork.png" -draw "image over 0,0 512,512 'Assets/Beta-Overlay.png'" "Assets/iTunesArtwork-beta.png"
 convert "Assets/iTunesArtwork@2x.png" -draw "image over 0,0 1024,1024 'Assets/Beta-Overlay.png'" "Assets/iTunesArtwork-beta@2x.png"
+
+convert "Assets/Icon.png" -draw "image over 0,0 57,57 'Assets/Dev-Overlay.png'" "Assets/Icon-dev.png"
+convert "Assets/Icon@2x.png" -draw "image over 0,0 114,114 'Assets/Dev-Overlay.png'" "Assets/Icon-dev@2x.png"
+convert "Assets/Icon-settings.png" -draw "image over 0,0 29,29 'Assets/Dev-Overlay.png'" "Assets/Icon-settings-dev.png"
+convert "Assets/Icon-settings@2x.png" -draw "image over 0,0 58,58 'Assets/Dev-Overlay.png'" "Assets/Icon-settings-dev@2x.png"
+convert "Assets/iTunesArtwork.png" -draw "image over 0,0 512,512 'Assets/Dev-Overlay.png'" "Assets/iTunesArtwork-dev.png"
+convert "Assets/iTunesArtwork@2x.png" -draw "image over 0,0 1024,1024 'Assets/Dev-Overlay.png'" "Assets/iTunesArtwork-dev@2x.png"
