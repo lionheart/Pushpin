c6173f300bae5a31887b0c0d928e4da9e122ae2d
Dan Loewenherz <dloewenherz@gmail.com>
2013-01-29 16:24:38 -0800

    iEYEABECAAYFAlEIaFEACgkQbq8ASwDlvWBMrACgm8WJd4ad7mydwlF/xG+FWQBt  /sUAoNTo3t9Ar6ceFNM5Fh5Z2CToxCaC  =yKCO  -----END PGP SIGNATURE-----
    add login timeout

--- Pinboard for iPhone/LoginViewController.h
+++ Pinboard for iPhone/LoginViewController.h
@@ -19,9 +19,17 @@
 @property (nonatomic, retain) UITextView *textView;
 @property (nonatomic, retain) UIProgressView *progressView;
 @property (nonatomic, retain) UITextField *textField;
+@property (nonatomic) BOOL loginRequestInProgress;
+@property (nonatomic) CGRect activityIndicatorFrameTop;
+@property (nonatomic) CGRect activityIndicatorFrameBottom;
+@property (nonatomic, retain) NSURLConnection *loginConnection;
+@property (nonatomic, retain) NSTimer *loginTimer;
 
 - (void)keyboardWasShown:(NSNotification *)notification;
 - (void)keyboardWasHidden:(NSNotification *)notification;
 - (void)login;
+- (void)cancelLogin;
+- (void)resetLoginScreen;
+- (void)loginFailed;
 
 @end
