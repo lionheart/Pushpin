dcfa677d920f03bff8112f2e608caecb91987e41
Dan Loewenherz <dloewenherz@gmail.com>
2013-01-30 22:19:00 -0800

    use ASPinboard login in loginviewcontroller

--- Pinboard for iPhone/LoginViewController.h
+++ Pinboard for iPhone/LoginViewController.h
@@ -19,10 +19,8 @@
 @property (nonatomic, retain) UITextView *textView;
 @property (nonatomic, retain) UIProgressView *progressView;
 @property (nonatomic, retain) UITextField *textField;
-@property (nonatomic) BOOL loginRequestInProgress;
 @property (nonatomic) CGRect activityIndicatorFrameTop;
 @property (nonatomic) CGRect activityIndicatorFrameBottom;
-@property (nonatomic, retain) NSURLConnection *loginConnection;
 @property (nonatomic, retain) NSTimer *loginTimer;
 
 - (void)keyboardWasShown:(NSNotification *)notification;
