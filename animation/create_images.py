7d054d9381ca435f18649c0a7c929ea8b80cfbc0
Dan Loewenherz <dloewenherz@gmail.com>
2013-03-19 13:17:32 -0700

    add black stripes back

--- animation/create_images.py
+++ animation/create_images.py
@@ -38,17 +38,24 @@ for i in range(count):
     im.putalpha(mask)
     im.save("../Assets/PullToRefresh/ptr_{0:02d}@2x.png".format(count - i))
 
-"""
 # Loading animation
 count = 20
 for i in range(count):
     mask = Image.open("mask.bmp").convert('L')
 
-    im = Image.new("L", (80, 79))
+    im = Image.new("L", (40, 40))
 
-    box = (0, i - 10, 80, 79 + i)
+    box = (0, i - 15, 40, 30 + i)
     im.paste(Image.open("stripes.bmp"), box)
     im.putalpha(mask)
-    im.save("loading_{0:02d}.png".format(count - i))
+    im.save("../Assets/PullToRefreshAnimating/loading_{0:02d}.png".format(count - i))
 
-"""
+    mask = Image.open("mask@2x.bmp").convert('L')
+
+    im = Image.new("L", (80, 80))
+
+    box = (0, i - 10, 80, 80 + i)
+    im.paste(Image.open("stripes@2x.bmp"), box)
+    im.putalpha(mask)
+    im.save("../Assets/PullToRefreshAnimating/loading_{0:02d}@2x.png".format(count - i))
+
