dbe8361961882e7019fe1590b8b0209fdc842cce
Dan Loewenherz <dloewenherz@gmail.com>
2013-05-11 15:49:13 -0700

    update loading image

---
+++ animation/create_fill_loading.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+from PIL import Image
+from PIL import ImageDraw
+import math
+
+# Loading animation
+count = 80
+interval = (2 * math.pi) / count
+for i in range(count):
+    mask = Image.open("mask.bmp").convert('L')
+
+    im = Image.new("L", (40, 40))
+
+    box = (0, i - 15, 40, 30 + i)
+    math.cos(i)
+
+    draw = ImageDraw.Draw(im)
+    draw.rectangle([(0, 0), (40, 20 + 20 * math.cos(interval * i))], fill=255)
+
+    im.putalpha(mask)
+    im.save("../Assets/PullToRefreshAnimating/loading_{0:02d}.png".format(count - i))
+
+    mask = Image.open("mask@2x.bmp").convert('L')
+
+    im = Image.new("L", (80, 80))
+
+    draw = ImageDraw.Draw(im)
+    draw.rectangle([(0, 0), (80, 40 + 40 * math.cos(interval * i))], fill=255)
+
+    box = (0, i - 10, 80, 80 + i)
+    im.putalpha(mask)
+    im.save("../Assets/PullToRefreshAnimating/loading_{0:02d}@2x.png".format(count - i))
+
