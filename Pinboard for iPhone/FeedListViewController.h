2b048f05625d2eeca526c896d3d8e49aac0972f9
Dan Loewenherz <dloewenherz@gmail.com>
2013-05-19 16:33:48 -0700

    add PPTableViewController

--- Pinboard for iPhone/FeedListViewController.h
+++ Pinboard for iPhone/FeedListViewController.h
@@ -7,6 +7,7 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "PPTableViewController.h"
 
 enum PINBOARD_FEED_ITEMS {
     PinboardFeedAllBookmarks,
@@ -17,7 +18,7 @@ enum PINBOARD_FEED_ITEMS {
     PinboardFeedStarredBookmarks
 };
 
-@interface FeedListViewController : UITableViewController
+@interface FeedListViewController : PPTableViewController
 
 @property (nonatomic) BOOL connectionAvailable;
 @property (nonatomic, retain) UINavigationController *navigationController;
---
+++ Pinboard for iPhone/PPTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  PPTableViewController.h
+//  Pushpin
+//
+//  Created by Dan Loewenherz on 5/19/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPTableViewController : UITableViewController
+
+@end
