cd9fea9848319075f5542ec78ee997e81ad4e016
Dan Loewenherz <dloewenherz@gmail.com>
2013-05-24 14:19:22 -0700

    remove interface orientation support

--- Pinboard for iPhone/PPToolbar.m
+++ Pinboard for iPhone/PPToolbar.m
@@ -14,12 +14,10 @@
 - (id)initWithFrame:(CGRect)frame {
     self = [super initWithFrame:frame];
     if (self) {
-        UIImage *backgroundImage = [UIColor colorWithRed:0.161 green:0.176 blue:0.318 alpha:1];
-
         CALayer *topBorder = [CALayer layer];
         topBorder.frame = CGRectMake(0, 0, 600, 1);
         topBorder.borderWidth = 1;
-        topBorder.borderColor = [UIColor blackColor].CGColor;
+        topBorder.borderColor = [UIColor colorWithRed:0.161 green:0.176 blue:0.318 alpha:1].CGColor;
         [self.layer insertSublayer:topBorder atIndex:[self.layer.sublayers count]];
     }
     return self;
