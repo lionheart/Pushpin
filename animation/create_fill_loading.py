3bb6544d6cc1134b7c5f75ad3f5842689dd3e5fe
Dan Loewenherz <dloewenherz@gmail.com>
2013-05-26 21:20:22 -0700

    update rgb

--- animation/create_fill_loading.py
+++ animation/create_fill_loading.py
@@ -17,7 +17,7 @@ for i in range(count):
 
     draw = ImageDraw.Draw(im)
     draw.rectangle([(0, 0), (40, 20 + 20 * math.cos(interval * i))], fill="rgb(76, 88, 106)")
-    draw.rectangle([(0, 40), (40, 20 + 20 * math.cos(interval * i))], fill="rgb(217,224,233)")
+    draw.rectangle([(0, 40), (40, 20 + 20 * math.cos(interval * i))], fill="rgb(217, 224, 233)")
 
     im.putalpha(mask)
     im.save("../Assets/PullToRefreshAnimating/loading_{0:02d}.png".format(count - i))
@@ -28,7 +28,7 @@ for i in range(count):
 
     draw = ImageDraw.Draw(im)
     draw.rectangle([(0, 0), (80, 40 + 40 * math.cos(interval * i))], fill="rgb(76, 88, 106)")
-    draw.rectangle([(0, 80), (80, 40 + 40 * math.cos(interval * i))], fill="rgb(217,224,233)")
+    draw.rectangle([(0, 80), (80, 40 + 40 * math.cos(interval * i))], fill="rgb(217, 224, 233)")
 
     box = (0, i - 10, 80, 80 + i)
     im.putalpha(mask)
