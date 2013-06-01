d5273c1f2df3e3d54b6814fc3a57b9957e2b8aba
Dan Loewenherz <dloewenherz@gmail.com>
2013-06-01 13:40:53 -0700

    clean up about view controller

--- Pinboard for iPhone/PPAboutViewController.m
+++ Pinboard for iPhone/PPAboutViewController.m
@@ -51,13 +51,16 @@
                                 "\n\n2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution."
                                 "\n\nTHIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS \"AS IS\" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.", @"https://github.com/tonymillion/Reachability",],
                               @[@"oauthconsumer", [NSNull null], @"https://github.com/jdg/oauthconsumer"], @[@"RPSTPasswordManagementAppService", [NSNull null], @"https://github.com/Riposte/RPSTPasswordManagementAppService"]];
-        NSArray *description = @[@[@"About Pushpin", @"Pushpin is the product of overwhelming amounts of caffeine, 80's club music, and kittens. Lionheart Software builds beautiful applications for the iPhone and for the web."], @[@"Follow @dwlz on Twitter", [NSNull null]], @[@"Follow @Pushpin_app on Twitter", [NSNull null]], @[@"Review Pushpin on iTunes", [NSNull null]]];
-        self.data = @[description, credits, translations, beta, licenses];
-        self.titles = @[[NSNull null], @"Credits", @"Translations", @"Beta Testers", @"Software"];
+        NSArray *description = @[@[@"About Pushpin", @"Pushpin is the product of overwhelming amounts of caffeine, 80's club music, and kittens. Lionheart Software builds beautiful applications for the iPhone and for the web."], @[@"Follow Pushpin on Twitter", [NSNull null]], @[@"Review Pushpin on iTunes", [NSNull null]]];
+        NSArray *team = @[@[@"Dan Loewenherz", @"Product design and development.", @"dwlz"], @[@"Martin Karasek", @"Visual design.", [NSNull null]]];
+        self.data = @[description, team, credits, translations, beta, licenses];
+        self.titles = @[[NSNull null], @"Team", @"Credits", @"Translations", @"Beta Testers", @"Software"];
         self.expandedIndexPaths = [NSMutableArray arrayWithObject:[NSIndexPath indexPathForRow:0 inSection:0]];
         
         self.heights = [NSMutableDictionary dictionary];
         UIFont *font = [UIFont fontWithName:@"Avenir-Medium" size:16];
+        UIFont *fixedWidthFont = [UIFont fontWithName:@"Courier" size:12];
+        NSInteger index = 0;
         for (NSArray *list in self.data) {
             for (NSArray *pair in list) {
                 NSString *title = pair[0];
@@ -73,15 +76,18 @@
                     self.heights[description] = @(0);
                 }
                 else {
-                    self.heights[description] = @([description sizeWithFont:font constrainedToSize:CGSizeMake(280, CGFLOAT_MAX)].height);
+                    if (index == 5) {
+                        self.heights[description] = @([description sizeWithFont:fixedWidthFont constrainedToSize:CGSizeMake(280, CGFLOAT_MAX)].height);
+                    }
+                    else {
+                        self.heights[description] = @([description sizeWithFont:font constrainedToSize:CGSizeMake(280, CGFLOAT_MAX)].height);
+                    }
                 }
             }
+            index++;
         }
         
         self.loadingIndicator = [[PPLoadingView alloc] initWithFrame:CGRectMake(0, 0, 40, 40)];
-        
-        
-        
     }
     return self;
 }
@@ -159,6 +165,7 @@
     if (bottomHeight > 80 && ![self.expandedIndexPaths containsObject:indexPath]) {
         bottomHeight = 22;
     }
+
     return topHeight + bottomHeight + 20;
 }
 
@@ -194,7 +201,13 @@
     cell.accessoryView = nil;
     
     cell.textLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:16];
-    cell.detailTextLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:16];
+
+    if (indexPath.section == [self.titles indexOfObject:@"Software"]) {
+        cell.detailTextLabel.font = [UIFont fontWithName:@"Courier" size:12];
+    }
+    else {
+        cell.detailTextLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:16];
+    }
     cell.detailTextLabel.numberOfLines = 0;
     cell.textLabel.text = nil;
     cell.detailTextLabel.text = nil;
@@ -203,10 +216,7 @@
     if (indexPath.section == 0 && indexPath.row == 1) {
         cell.imageView.image = [UIImage imageNamed:@"twitter"];
     }
-    if (indexPath.section == 0 && indexPath.row == 2) {
-        cell.imageView.image = [UIImage imageNamed:@"twitter"];
-    }
-    if (indexPath.section == 0 && indexPath.row == 3) {
+    else if (indexPath.section == 0 && indexPath.row == 2) {
         cell.imageView.image = [UIImage imageNamed:@"apple"];
     }
     
@@ -228,7 +238,13 @@
         cell.textLabel.text = title;
     }
     if ([self.heights[detail] floatValue] > 80 && ![self.expandedIndexPaths containsObject:indexPath]) {
-        cell.detailTextLabel.text = @"Tap to expand.";
+        cell.detailTextLabel.font = [UIFont fontWithName:@"Avenir-Medium" size:16];
+        if (indexPath.section == [self.titles indexOfObject:@"Software"]) {
+            cell.detailTextLabel.text = @"Tap to view license.";
+        }
+        else {
+            cell.detailTextLabel.text = @"Tap to expand.";
+        }
     }
     else {
         if (![detail isEqual:[NSNull null]]) {
@@ -242,9 +258,6 @@
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
 
     if (indexPath.section == 0 && indexPath.row == 1) {
-        [self followScreenName:@"dwlz"];
-    }
-    else if (indexPath.section == 0 && indexPath.row == 2) {
         [self followScreenName:@"pushpin_app"];
     }
     else if (indexPath.section == 0 && indexPath.row == 3) {
@@ -266,90 +279,100 @@
 
 
 - (void)followScreenName:(NSString *)screenName {
-    ACAccountStore *accountStore = [[ACAccountStore alloc] init];
-    ACAccountType *twitter = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
-    
-    void (^AccessGrantedBlock)(WCAlertView *) = ^(WCAlertView *loadingAlertView) {
-        self.twitterAccountActionSheet = [[RDActionSheet alloc] initWithTitle:NSLocalizedString(@"Select Twitter Account:", nil) delegate:self cancelButtonTitle:NSLocalizedString(@"Cancel", nil) primaryButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:nil];
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        ACAccountStore *accountStore = [[ACAccountStore alloc] init];
+        ACAccountType *twitter = [accountStore accountTypeWithAccountTypeIdentifier:ACAccountTypeIdentifierTwitter];
+        
+        void (^AccessGrantedBlock)(WCAlertView *) = ^(WCAlertView *loadingAlertView) {
+            self.twitterAccountActionSheet = [[RDActionSheet alloc] initWithTitle:NSLocalizedString(@"Select Twitter Account:", nil) delegate:self cancelButtonTitle:NSLocalizedString(@"Cancel", nil) primaryButtonTitle:nil destructiveButtonTitle:nil otherButtonTitles:nil];
 
-        NSMutableDictionary *accounts = [NSMutableDictionary dictionary];
-        for (ACAccount *account in [accountStore accountsWithAccountType:twitter]) {
-            [self.twitterAccountActionSheet addButtonWithTitle:account.username];
-            [accounts setObject:account.identifier forKey:account.username];
-        }
-        
-        if (loadingAlertView) {
-            [loadingAlertView dismissWithClickedButtonIndex:0 animated:YES];
-        }
-        
-        void (^Tweet)(NSString *) = ^(NSString *username) {
-            ACAccount *account = [accountStore accountWithIdentifier:accounts[username]];
-            SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeTwitter
-                                                    requestMethod:SLRequestMethodPOST
-                                                              URL:[NSURL URLWithString:@"https://api.twitter.com/1.1/friendships/create.json"]
-                                                       parameters:@{@"screen_name": screenName, @"follow": @"true"}];
-            [request setAccount:account];
-            [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
-                [[AppDelegate sharedDelegate] setNetworkActivityIndicatorVisible:NO];
-                NSDictionary *response = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableContainers error:nil];
-                if (response[@"errors"]) {
-                    NSString *code = [NSString stringWithFormat:@"Error #%@", response[@"errors"][0][@"code"]];
-                    NSString *message = [NSString stringWithFormat:@"%@", response[@"errors"][0][@"message"]];
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        WCAlertView *alertView = [[WCAlertView alloc] initWithTitle:code message:message delegate:nil cancelButtonTitle:NSLocalizedString(@"Uh oh.", nil) otherButtonTitles:nil];
-                        [alertView show];
-                    });
-                }
-                else {
-                    dispatch_async(dispatch_get_main_queue(), ^{
-                        WCAlertView *alertView = [[WCAlertView alloc] initWithTitle:NSLocalizedString(@"Success", nil) message:[NSString stringWithFormat:@"You are now following @%@!", screenName] delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles:nil];
-                        [alertView show];
-                    });
-                }
-            }];
+            NSMutableDictionary *accounts = [NSMutableDictionary dictionary];
+            for (ACAccount *account in [accountStore accountsWithAccountType:twitter]) {
+                [self.twitterAccountActionSheet addButtonWithTitle:account.username];
+                [accounts setObject:account.identifier forKey:account.username];
+            }
+
+            if (loadingAlertView) {
+                [loadingAlertView dismissWithClickedButtonIndex:0 animated:YES];
+            }
+
+            void (^Tweet)(NSString *) = ^(NSString *username) {
+                ACAccount *account = [accountStore accountWithIdentifier:accounts[username]];
+                SLRequest *request = [SLRequest requestForServiceType:SLServiceTypeTwitter
+                                                        requestMethod:SLRequestMethodPOST
+                                                                  URL:[NSURL URLWithString:@"https://api.twitter.com/1.1/friendships/create.json"]
+                                                           parameters:@{@"screen_name": screenName, @"follow": @"true"}];
+                [request setAccount:account];
+                [request performRequestWithHandler:^(NSData *responseData, NSHTTPURLResponse *urlResponse, NSError *error) {
+                    [[AppDelegate sharedDelegate] setNetworkActivityIndicatorVisible:NO];
+                    NSDictionary *response = [NSJSONSerialization JSONObjectWithData:responseData options:NSJSONReadingMutableContainers error:nil];
+                    if (response[@"errors"]) {
+                        NSString *code = [NSString stringWithFormat:@"Error #%@", response[@"errors"][0][@"code"]];
+                        NSString *message = [NSString stringWithFormat:@"%@", response[@"errors"][0][@"message"]];
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            WCAlertView *alertView = [[WCAlertView alloc] initWithTitle:code message:message delegate:nil cancelButtonTitle:NSLocalizedString(@"Uh oh.", nil) otherButtonTitles:nil];
+                            [alertView show];
+                        });
+                    }
+                    else {
+                        dispatch_async(dispatch_get_main_queue(), ^{
+                            WCAlertView *alertView = [[WCAlertView alloc] initWithTitle:NSLocalizedString(@"Success", nil) message:[NSString stringWithFormat:@"You are now following @%@!", screenName] delegate:nil cancelButtonTitle:NSLocalizedString(@"OK", nil) otherButtonTitles:nil];
+                            [alertView show];
+                        });
+                    }
+                }];
+            };
+
+            if ([accounts count] == 0) {
+            }
+            else if ([accounts count] == 1) {
+                [[AppDelegate sharedDelegate] setNetworkActivityIndicatorVisible:YES];
+
+                ACAccount *account = [accountStore accountsWithAccountType:twitter][0];
+                Tweet(account.username);
+            }
+            else {
+                [[AppDelegate sharedDelegate] setNetworkActivityIndicatorVisible:YES];
+
+                self.twitterAccountActionSheet.callbackBlock = ^(RDActionSheetCallbackType result, NSInteger buttonIndex, NSString *buttonTitle) {
+                    if (result == RDActionSheetCallbackTypeClickedButtonAtIndex && ![buttonTitle isEqualToString:@"Cancel"]) {
+                        Tweet(buttonTitle);
+                    }
+                };
+
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    [self.twitterAccountActionSheet showFrom:self.navigationController.view];
+                });
+            }
         };
         
-        if ([accounts count] == 0) {
-        }
-        else if ([accounts count] == 1) {
-            [[AppDelegate sharedDelegate] setNetworkActivityIndicatorVisible:YES];
-            ACAccount *account = [accountStore accountsWithAccountType:twitter][0];
-            Tweet(account.username);
+        if (!twitter.accessGranted) {
+            dispatch_async(dispatch_get_main_queue(), ^{
+                WCAlertView *loadingAlertView = [[WCAlertView alloc] initWithTitle:@"Loading" message:@"Requesting access to your Twitter accounts." delegate:nil cancelButtonTitle:nil otherButtonTitles:nil];
+                [loadingAlertView show];
+
+                self.loadingIndicator.center = CGPointMake(loadingAlertView.bounds.size.width/2, loadingAlertView.bounds.size.height-45);
+                [self.loadingIndicator startAnimating];
+                [loadingAlertView addSubview:self.loadingIndicator];
+
+                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+                    [accountStore requestAccessToAccountsWithType:twitter
+                                                          options:nil
+                                                       completion:^(BOOL granted, NSError *error) {
+                                                           if (granted) {
+                                                               AccessGrantedBlock(loadingAlertView);
+                                                           }
+                                                           else {
+                                                               [loadingAlertView dismissWithClickedButtonIndex:0 animated:YES];
+                                                           }
+                                                       }];
+                });
+            });
         }
         else {
-            [[AppDelegate sharedDelegate] setNetworkActivityIndicatorVisible:YES];
-            
-            self.twitterAccountActionSheet.callbackBlock = ^(RDActionSheetCallbackType result, NSInteger buttonIndex, NSString *buttonTitle) {
-                if (result == RDActionSheetCallbackTypeClickedButtonAtIndex && ![buttonTitle isEqualToString:@"Cancel"]) {
-                    Tweet(buttonTitle);
-                }
-            };
-            
-            [self.twitterAccountActionSheet showFrom:self.navigationController.view];
+            AccessGrantedBlock(nil);
         }
-    };
-    
-    if (!twitter.accessGranted) {
-        WCAlertView *loadingAlertView = [[WCAlertView alloc] initWithTitle:@"Loading" message:@"Requesting access to your Twitter accounts." delegate:nil cancelButtonTitle:nil otherButtonTitles:nil];
-        [loadingAlertView show];
-        
-        self.loadingIndicator.center = CGPointMake(loadingAlertView.bounds.size.width/2, loadingAlertView.bounds.size.height-45);
-        [self.loadingIndicator startAnimating];
-        [loadingAlertView addSubview:self.loadingIndicator];
-        [accountStore requestAccessToAccountsWithType:twitter
-                                              options:nil
-                                           completion:^(BOOL granted, NSError *error) {
-                                               if (granted) {
-                                                   AccessGrantedBlock(loadingAlertView);
-                                               }
-                                               else {
-                                                   [loadingAlertView dismissWithClickedButtonIndex:0 animated:YES];
-                                               }
-                                           }];
-    }
-    else {
-        AccessGrantedBlock(nil);
-    }
+    });
 }
 
 - (void)copyURL:(id)sender {
