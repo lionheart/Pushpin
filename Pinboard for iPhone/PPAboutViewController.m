7e40654d3f36eaa96b3b3ac625e9cd8df44492a9
Dan Loewenherz <dloewenherz@gmail.com>
2013-05-31 18:11:05 -0700

    fix unread/read bug

--- Pinboard for iPhone/PPAboutViewController.m
+++ Pinboard for iPhone/PPAboutViewController.m
@@ -25,7 +25,7 @@
     self = [super initWithStyle:UITableViewStyleGrouped];
     if (self) {
         NSArray *credits = @[@[@"Rachel", @"For always believing in me."], @[@"Dante and Isabelle", @"For inspiring me and for making me laugh."], @[@"Maciej Ceglowski", @"For making Pinboard."]];
-        NSArray *beta = @[@[@"Michael Solis", [NSNull null], @"morphopod"], @[@"Phil Havens", [NSNull null], @"philhavens"], @[@"Carolina Bertazzo Currat", [NSNull null], [NSNull null]]];
+        NSArray *beta = @[@[@"Michael Solis", [NSNull null], @"morphopod"], @[@"Phil Havens", [NSNull null], @"philhavens"]];
         NSArray *translations = @[@[@"Riccardo Mori", @"Italian", @"morrick"], @[@"James Lepthien", @"German", @"0x86DD"], @[@"Jérôme Tomasini", @"French", @"c0wb0yz"], @[@"Vítor Galvão", @"Portuguese", @"vhgalvao"]];
         NSArray *licenses = @[@[@"TTTAttributedLabel", @"Copyright (c) 2011 Mattt Thompson (http://mattt.me/)"
                                 "\n\nPermission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the \"Software\"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:"
