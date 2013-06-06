e5a2e1b704c4b4f7f7d3e4e86b5c40b6fb63f81e
Dan Loewenherz <dloewenherz@gmail.com>
2013-06-05 18:15:46 -0700

    add changelog

---
+++ Pinboard for iPhone/PPChangelogViewController.h
@@ -0,0 +1,17 @@
+//
+//  PPChangelogViewController.h
+//  Pushpin
+//
+//  Created by Dan Loewenherz on 6/5/13.
+//
+//
+
+#import "PPTableViewController.h"
+
+@interface PPChangelogViewController : PPTableViewController
+
+@property (nonatomic, strong) NSArray *data;
+@property (nonatomic, strong) NSMutableArray *titles;
+@property (nonatomic, strong) NSMutableDictionary *heights;
+
+@end
