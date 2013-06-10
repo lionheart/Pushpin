5fa1e3728dd9ff7a75f1c399f89e2086875a222f
Dan Loewenherz <dloewenherz@gmail.com>
2013-06-10 15:32:38 -0700

    more fixes for full screen mode, refs #128

--- Pinboard for iPhone/PPWebViewController.h
+++ Pinboard for iPhone/PPWebViewController.h
@@ -21,16 +21,23 @@
 @property (nonatomic, strong) UIBarButtonItem *readerBarButtonItem;
 @property (nonatomic, strong) UIBarButtonItem *actionBarButtonItem;
 @property (nonatomic, strong) UIBarButtonItem *socialBarButtonItem;
+@property (nonatomic, strong) UIBarButtonItem *activityIndicatorBarButtonItem;
+@property (nonatomic, strong) UIActivityIndicatorView *activityIndicator;
 @property (nonatomic, strong) UIButton *readerButton;
+@property (nonatomic, strong) UIButton *enterReaderModeButton;
+@property (nonatomic, strong) UIButton *exitReaderModeButton;
+@property (nonatomic, strong) CALayer *fullScreenImageLayer;
+@property (nonatomic, strong) UIPanGestureRecognizer *panGestureRecognizerForReaderMode;
+@property (nonatomic, strong) UIPanGestureRecognizer *panGestureRecognizerForNormalMode;
 @property (nonatomic, strong) NSTimer *stoppedScrollingTimer;
 @property (nonatomic, strong) PPToolbar *toolbar;
-@property (nonatomic, strong) UISwipeGestureRecognizer *rightSwipeGestureRecognizer;
 @property (nonatomic, strong) UITapGestureRecognizer *singleTapGestureRecognizer;
 @property (nonatomic) NSInteger numberOfRequestsInProgress;
 @property (nonatomic) BOOL alreadyLoaded;
 @property (nonatomic) BOOL stopped;
 @property (nonatomic) CGFloat lastContentOffset;
 
+- (void)gestureDetected:(UIGestureRecognizer *)recognizer;
 - (void)singleTapInWebview;
 - (void)socialActionButtonTouchUp:(id)sender;
 - (void)actionButtonTouchUp:(id)sender;
@@ -49,6 +56,8 @@
 - (void)loadURL;
 - (void)stopLoading;
 - (BOOL)isWebViewExpanded;
+- (CGPoint)adjustedPuckPositionWithPoint:(CGPoint)point;
+- (void)toggleFullScreen;
 - (NSURL *)url;
 - (NSString *)urlStringForDemobilizedURL:(NSURL *)url;
 - (void)expandWebViewToFullScreen;
