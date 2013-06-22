c6890ef3f741eac766b38a70ed985d9ba6065c14
Dan Loewenherz <dloewenherz@gmail.com>
2013-06-21 22:50:28 -0700

    rearrange implementation files

--- Pinboard for iPhone/UIApplication+Additions.m
+++ Pinboard for iPhone/UIApplication+Additions.m
@@ -8,9 +8,36 @@
 
 #import <UIKit/UIKit.h>
 
-@interface UIApplication (Additions)
 
-+ (BOOL)isIPad;
-+ (BOOL)isIOS6OrGreater;
+#import "UIApplication+Additions.h"
+
+@implementation UIApplication (Additions)
+
++ (BOOL)isIPad {
+    __block BOOL isIPad;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        isIPad = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad;
+    });
+    return isIPad;
+}
+
++ (BOOL)isIOS6OrGreater {
+    return [[[UIDevice currentDevice] systemVersion] floatValue] >= 6.0;
+}
++ (CGSize)currentSize {
+    return [UIApplication sizeInOrientation:[UIApplication sharedApplication].statusBarOrientation];
+}
+
++ (CGSize)sizeInOrientation:(UIInterfaceOrientation)orientation {
+    CGSize size = [UIScreen mainScreen].bounds.size;
+    UIApplication *application = [UIApplication sharedApplication];
+    
+    if (UIInterfaceOrientationIsLandscape(orientation)) {
+        size = CGSizeMake(size.height, size.width);
+    }
+
+    return size;
+}
 
 @end
