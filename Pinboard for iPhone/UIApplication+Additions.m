c9dac6343776a6d29bd0d76f6a6ddda3bfe98696
Dan Loewenherz <dloewenherz@gmail.com>
2013-06-22 15:41:49 -0700

    minor fixes in additions

--- Pinboard for iPhone/UIApplication+Additions.m
+++ Pinboard for iPhone/UIApplication+Additions.m
@@ -14,24 +14,19 @@
 @implementation UIApplication (Additions)
 
 + (BOOL)isIPad {
-    __block BOOL isIPad;
-    static dispatch_once_t onceToken;
-    dispatch_once(&onceToken, ^{
-        isIPad = UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad;
-    });
-    return isIPad;
+    return UI_USER_INTERFACE_IDIOM() == UIUserInterfaceIdiomPad;
 }
 
 + (BOOL)isIOS6OrGreater {
     return [[[UIDevice currentDevice] systemVersion] floatValue] >= 6.0;
 }
+
 + (CGSize)currentSize {
     return [UIApplication sizeInOrientation:[UIApplication sharedApplication].statusBarOrientation];
 }
 
 + (CGSize)sizeInOrientation:(UIInterfaceOrientation)orientation {
     CGSize size = [UIScreen mainScreen].bounds.size;
-    UIApplication *application = [UIApplication sharedApplication];
     
     if (UIInterfaceOrientationIsLandscape(orientation)) {
         size = CGSizeMake(size.height, size.width);
