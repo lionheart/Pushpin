b8917065e9ba9589c9aa767bcb676563e8e9ff7d
Dan Loewenherz <dloewenherz@gmail.com>
2013-07-13 22:56:05 -0700

    add shake to view random bookmark, closes #186

--- Pinboard for iPhone/PPNavigationController.h
+++ Pinboard for iPhone/PPNavigationController.h
@@ -7,7 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "AppDelegate.h"
 
-@interface PPNavigationController : UINavigationController
+@interface PPNavigationController : UINavigationController <ModalDelegate>
+
+@property (nonatomic, strong) PPNavigationController *randomBookmarkNavigationController;
 
 @end
