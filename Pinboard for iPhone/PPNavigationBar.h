9350e1937537f5ced8f8a0438797ae4c9b18c023
Andy Muldowney <andymuldowney@gmail.com>
2013-11-21 10:51:44 -0600

    Navigaiton bar tint color

---
+++ Pinboard for iPhone/PPNavigationBar.h
@@ -0,0 +1,15 @@
+//
+//  PPNavigationBar.h
+//  Pushpin
+//
+//  Created by Andy Muldowney on 11/20/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PPNavigationBar : UINavigationBar
+
+@property (nonatomic, strong) CALayer *colorLayer;
+
+@end
