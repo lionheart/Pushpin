8bf66e738d1292d29fd0ebd8ed63ba0894150ee4
Dan Loewenherz <dloewenherz@gmail.com>
2013-12-18 11:17:13 -0600

    add metadata object to clean up metadata methods

---
+++ Pinboard for iPhone/PostMetadata.h
@@ -0,0 +1,18 @@
+//
+//  PostMetadata.h
+//  Pushpin
+//
+//  Created by Dan Loewenherz on 12/18/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PostMetadata : NSObject
+
+@property (nonatomic, strong) NSAttributedString *string;
+@property (nonatomic, strong) NSNumber *height;
+@property (nonatomic, strong) NSArray *links;
+@property (nonatomic, strong) NSArray *badges;
+
+@end
---
+++ Pinboard for iPhone/PostMetadata.m
@@ -0,0 +1,13 @@
+//
+//  PostMetadata.m
+//  Pushpin
+//
+//  Created by Dan Loewenherz on 12/18/13.
+//
+//
+
+#import "PostMetadata.h"
+
+@implementation PostMetadata
+
+@end
