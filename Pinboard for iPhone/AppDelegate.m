9a93ea6adba8f3549f8656348e5ffd4ce9ad012e
Dan Loewenherz <dloewenherz@gmail.com>
2013-12-17 22:20:18 -0600

    set ontintcolor of uiswitches

--- Pinboard for iPhone/AppDelegate.m
+++ Pinboard for iPhone/AppDelegate.m
@@ -292,6 +292,7 @@
     UIColor *barButtonItemColor = [UIColor colorWithRed:40/255.0f green:141/255.0f blue:219/255.0f alpha:1.0f];
     [[UIBarButtonItem appearanceWhenContainedIn:[UIToolbar class], nil] setTintColor:barButtonItemColor];
     
+    [[UISwitch appearance] setOnTintColor:HEX(0x0096FFFF)];
     [[UINavigationBar appearance] setBarTintColor:[UIColor colorWithRed:0 green:0.5863 blue:1 alpha:1]];
     [[UINavigationBar appearance] setTintColor:[UIColor whiteColor]];
     [[UINavigationBar appearance] setTitleTextAttributes:@{NSForegroundColorAttributeName: [UIColor whiteColor]}];
