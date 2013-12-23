34d7339dd8a3c45033b581e38eb1863d1009bb0c
Dan Loewenherz <dloewenherz@gmail.com>
2013-12-23 13:59:13 -0600

    make login view controller a bit better, fix 1password integration

--- Pinboard for iPhone/LoginViewController.m
+++ Pinboard for iPhone/LoginViewController.m
@@ -30,14 +30,13 @@
 
 static NSString *LoginTableCellIdentifier = @"LoginTableViewCell";
 
-@synthesize activityIndicator;
-@synthesize textView;
-@synthesize progressView;
-@synthesize usernameTextField;
-@synthesize passwordTextField;
-@synthesize activityIndicatorFrameBottom;
-@synthesize activityIndicatorFrameTop;
-@synthesize onePasswordButton;
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
+}
+
+- (id)initWithStyle:(UITableViewStyle)style {
+    return [super initWithStyle:UITableViewStyleGrouped];
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
@@ -53,7 +52,9 @@ static NSString *LoginTableCellIdentifier = @"LoginTableViewCell";
     self.progressView = [[UIProgressView alloc] initWithProgressViewStyle:UIProgressViewStyleBar];
     self.progressView.translatesAutoresizingMaskIntoConstraints = NO;
     self.progressView.hidden = YES;
-    
+
+    NSDictionary *textViewAttributes = @{NSFontAttributeName: [PPTheme urlFont]};
+    NSAttributedString *textViewAttributedString = [[NSAttributedString alloc] initWithString:NSLocalizedString(@"Enter your Pinboard credentials above. Email support@lionheartsw.com if you have any issues.", nil) attributes:textViewAttributes];
     self.textView = [[UITextView alloc] init];
     self.textView.translatesAutoresizingMaskIntoConstraints = NO;
     self.textView.backgroundColor = [UIColor clearColor];
@@ -61,51 +62,7 @@ static NSString *LoginTableCellIdentifier = @"LoginTableViewCell";
     self.textView.editable = NO;
     self.textView.userInteractionEnabled = NO;
     self.textView.textAlignment = NSTextAlignmentCenter;
-    self.textView.font = [UIFont preferredFontForTextStyle:UIFontTextStyleCaption1];
-    self.textView.text = NSLocalizedString(@"Enter your Pinboard credentials above. Email support@lionheartsw.com if you have any issues.", nil);
-
-    CGSize size = [UIApplication currentSize];
-    CGFloat radius = 5;
-    CGRect buttonRect = CGRectMake(0, 0, radius * 2, 30);
-    CAGradientLayer *barButtonItemLayer = [CAGradientLayer layer];
-    barButtonItemLayer.frame = buttonRect;
-    barButtonItemLayer.cornerRadius = radius;
-    barButtonItemLayer.masksToBounds = YES;
-    barButtonItemLayer.borderWidth = 0.5;
-    barButtonItemLayer.borderColor = HEX(0x4C586AFF).CGColor;
-    barButtonItemLayer.colors = @[(id)HEX(0xFDFDFDFF).CGColor, (id)HEX(0xCED4E0FF).CGColor];
-    barButtonItemLayer.startPoint = CGPointMake(0.5, 0);
-    barButtonItemLayer.endPoint = CGPointMake(0.5, 1.0);
-    
-    UIGraphicsBeginImageContextWithOptions(buttonRect.size, NO, 0);
-    CGContextRef context = UIGraphicsGetCurrentContext();
-    [barButtonItemLayer renderInContext:context];
-    UIImage *barButtonBackground = [UIGraphicsGetImageFromCurrentImageContext() stretchableImageWithLeftCapWidth:radius topCapHeight:15];
-    UIGraphicsEndImageContext();
-    
-    UIGraphicsBeginImageContextWithOptions(buttonRect.size, NO, 0);
-    context = UIGraphicsGetCurrentContext();
-    barButtonItemLayer.colors = @[(id)HEX(0xCED4E0FF).CGColor, (id)HEX(0xFDFDFDFF).CGColor];
-    [barButtonItemLayer renderInContext:context];
-    UIImage *barButtonBackgroundHighlighted = [UIGraphicsGetImageFromCurrentImageContext() stretchableImageWithLeftCapWidth:radius topCapHeight:15];
-    UIGraphicsEndImageContext();
-    
-    self.onePasswordButton = [UIButton buttonWithType:UIButtonTypeCustom];
-    [self.onePasswordButton setFrame:CGRectMake((size.width - 180) / 2, 150, 180, 44)];
-    [self.onePasswordButton setTitle:@"Launch 1Password" forState:UIControlStateNormal];
-    [self.onePasswordButton setImage:[UIImage imageNamed:@"1P-29"] forState:UIControlStateNormal];
-    [self.onePasswordButton setImage:[UIImage imageNamed:@"1P-29"] forState:UIControlStateHighlighted];
-    [self.onePasswordButton setImageEdgeInsets:UIEdgeInsetsMake(0, 0, 0, radius)];
-    [self.onePasswordButton setTitleEdgeInsets:UIEdgeInsetsMake(0, radius, 0, 0)];
-    [self.onePasswordButton setBackgroundImage:barButtonBackground forState:UIControlStateNormal];
-    [self.onePasswordButton setBackgroundImage:barButtonBackgroundHighlighted forState:UIControlStateHighlighted];
-    [self.onePasswordButton addTarget:self action:@selector(sendToOnePassword) forControlEvents:UIControlEventTouchUpInside];
-
-    self.onePasswordButton.titleLabel.font = [UIFont fontWithName:[PPTheme boldFontName] size:15];
-    [self.onePasswordButton setTitleColor:HEX(0x4A5768FF) forState:UIControlStateNormal];
-    [self.onePasswordButton setTitleShadowColor:HEX(0xFFFFFF00) forState:UIControlStateNormal];
-    self.onePasswordButton.hidden = YES;
-    [self.view addSubview:self.onePasswordButton];
+    self.textView.attributedText = textViewAttributedString;
     
     self.usernameTextField = [[UITextField alloc] init];
     self.usernameTextField.translatesAutoresizingMaskIntoConstraints = NO;
@@ -138,7 +95,6 @@ static NSString *LoginTableCellIdentifier = @"LoginTableViewCell";
     self.loginTimer = nil;
     
     [self resetLoginScreen];
-
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(progressNotificationReceived:) name:kPinboardDataSourceProgressNotification object:nil];
 }
 
@@ -190,7 +146,6 @@ static NSString *LoginTableCellIdentifier = @"LoginTableViewCell";
     [self.navigationItem.rightBarButtonItem setEnabled:YES];
 
     if ([RPSTPasswordManagementAppService passwordManagementAppIsAvailable]) {
-        self.onePasswordButton.hidden = NO;
         self.textView.hidden = YES;
     }
 }
@@ -240,7 +195,6 @@ static NSString *LoginTableCellIdentifier = @"LoginTableViewCell";
             self.usernameTextField.textColor = [UIColor grayColor];
             self.passwordTextField.enabled = NO;
             self.passwordTextField.textColor = [UIColor grayColor];
-            self.onePasswordButton.hidden = YES;
             self.textView.hidden = NO;
             self.textView.text = NSLocalizedString(@"Verifying your credentials...", nil);
         
@@ -256,7 +210,7 @@ static NSString *LoginTableCellIdentifier = @"LoginTableViewCell";
                                                self.progressView.hidden = NO;
                                                
                                                [self.tableView beginUpdates];
-                                               [self.tableView deleteRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:0 inSection:0], [NSIndexPath indexPathForRow:1 inSection:0]] withRowAnimation:UITableViewRowAnimationAutomatic];
+                                               [self.tableView deleteRowsAtIndexPaths:@[[NSIndexPath indexPathForRow:kLoginUsernameRow inSection:0], [NSIndexPath indexPathForRow:kLoginPasswordRow inSection:0]] withRowAnimation:UITableViewRowAnimationAutomatic];
                                                [self.tableView endUpdates];
 
                                                dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
@@ -316,14 +270,11 @@ static NSString *LoginTableCellIdentifier = @"LoginTableViewCell";
     }
 }
 
-- (void)sendToOnePassword {
-    [[UIApplication sharedApplication] openURL:[RPSTPasswordManagementAppService passwordManagementAppCompleteURLForSearchQuery:@"pinboard"]];
-}
-
 - (BOOL)textFieldShouldReturn:(UITextField *)textField {
     if (textField == self.usernameTextField) {
         [self.passwordTextField becomeFirstResponder];
-    } else if (textField == self.passwordTextField) {
+    }
+    else if (textField == self.passwordTextField) {
         [self.passwordTextField resignFirstResponder];
         [self login];
     }
@@ -334,71 +285,122 @@ static NSString *LoginTableCellIdentifier = @"LoginTableViewCell";
 #pragma mark - Table view data source
 
 - (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 1;
+    return 2;
+    if ([[UIApplication sharedApplication] canOpenURL:[RPSTPasswordManagementAppService passwordManagementAppCompleteURLForSearchQuery:@"pinboard"]]) {
+        return 2;
+    }
+    else {
+        return 1;
+    }
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     if (self.progressView.hidden) {
-        return 2;
+        switch (section) {
+            case 0:
+                return 2;
+                
+            case 1:
+                return 1;
+        }
     }
-    else {
-        return 0;
-    }
+
+    return 0;
 }
 
 - (CGFloat)tableView:(UITableView *)tableView heightForFooterInSection:(NSInteger)section {
-    return 100;
+    if (section == 0) {
+        CGRect rect = [self.textView.attributedText boundingRectWithSize:CGSizeMake(300, CGFLOAT_MAX) options:NSStringDrawingUsesLineFragmentOrigin context:nil];
+        return rect.size.height + 14;
+    }
+    return 0;
 }
 
 - (UIView *)tableView:(UITableView *)tableView viewForFooterInSection:(NSInteger)section {
-    UIView *view = [[UIView alloc] init];
-    view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
-    
-    [view addSubview:self.progressView];
-    [view addSubview:self.textView];
-    
-    NSDictionary *views = @{@"progress": self.progressView,
-                            @"text": self.textView };
-    [view lhs_addConstraints:@"V:|[progress]-5-[text]-5-|" views:views];
-    [view lhs_addConstraints:@"H:|[progress]|" views:views];
-    [view lhs_addConstraints:@"H:|-5-[text]-5-|" views:views];
-    return view;
+    if (section == 0) {
+        UIView *view = [[UIView alloc] init];
+        view.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+
+        [view addSubview:self.progressView];
+        [view addSubview:self.textView];
+
+        NSDictionary *views = @{@"progress": self.progressView,
+                                @"text": self.textView };
+        
+
+        CGFloat height = 3;
+        if (self.progressView.hidden) {
+            height = 0;
+        }
+        NSDictionary *metrics = @{@"height": @(height)};
+        [view lhs_addConstraints:@"V:|[progress(height)]-5-[text]-5-|" metrics:metrics views:views];
+        [view lhs_addConstraints:@"H:|[progress]|" views:views];
+        [view lhs_addConstraints:@"H:|-5-[text]-5-|" views:views];
+        return view;
+    }
+    return nil;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:LoginTableCellIdentifier forIndexPath:indexPath];
+
+    [cell.contentView lhs_removeSubviews];
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
 
-    NSArray *subviews = [cell.contentView subviews];
-    for (UIView *subview in subviews) {
-        [subview removeFromSuperview];
-    }
-
-    UITextField *textField;
-    if (indexPath.row == 0) {
-        textField = self.usernameTextField;
-    }
-    else {
-        textField = self.passwordTextField;
+    switch (indexPath.section) {
+        case 0:
+            switch (indexPath.row) {
+                case kLoginUsernameRow: {
+                    NSDictionary *views = @{@"view": self.usernameTextField };
+                    [cell.contentView addSubview:self.usernameTextField];
+                    [cell.contentView lhs_addConstraints:@"H:|-15-[view]-5-|" views:views];
+                    [cell.contentView lhs_addConstraints:@"V:|-5-[view]-5-|" views:views];
+                    break;
+                }
+                    
+                case kLoginPasswordRow: {
+                    NSDictionary *views = @{@"view": self.passwordTextField };
+                    [cell.contentView addSubview:self.passwordTextField];
+                    [cell.contentView lhs_addConstraints:@"H:|-15-[view]-5-|" views:views];
+                    [cell.contentView lhs_addConstraints:@"V:|-5-[view]-5-|" views:views];
+                    break;
+                }
+            }
+            break;
+            
+        case 1: {
+            UIImageView *imageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"56_Key-alt"]];
+            cell.accessoryView = imageView;
+            cell.textLabel.text = @"Launch 1Password";
+            break;
+        }
     }
     
-    NSDictionary *views = @{
-                            @"text": textField };
-    [cell.contentView addSubview:textField];
-    [cell.contentView lhs_addConstraints:@"H:|-15-[text]-5-|" views:views];
-    [cell.contentView lhs_addConstraints:@"V:|-5-[text]-5-|" views:views];
     return cell;
 }
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     [tableView deselectRowAtIndexPath:indexPath animated:NO];
     
-    if (indexPath.row == 0) {
-        [self.usernameTextField becomeFirstResponder];
+    switch (indexPath.section) {
+        case 0:
+            switch (indexPath.row) {
+                case kLoginUsernameRow:
+                    [self.usernameTextField becomeFirstResponder];
+                    break;
+                    
+                case kLoginPasswordRow:
+                    [self.passwordTextField becomeFirstResponder];
+                    break;
+            }
+
+            break;
+            
+        case 1:
+            [[UIApplication sharedApplication] openURL:[RPSTPasswordManagementAppService passwordManagementAppCompleteURLForSearchQuery:@"pinboard"]];
+            break;
     }
-    else {
-        [self.passwordTextField becomeFirstResponder];
-    }
+    
 }
 
 - (void)showContactForm {
