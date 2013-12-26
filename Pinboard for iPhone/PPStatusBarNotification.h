f05d4608da6d0322299920575cb7ff555e7c1265
Dan Loewenherz <dloewenherz@gmail.com>
2013-12-26 12:54:00 -0600

    add right and left slide status bar notifications

--- Pinboard for iPhone/PPStatusBarNotification.h
+++ Pinboard for iPhone/PPStatusBarNotification.h
@@ -8,9 +8,18 @@
 
 #import <Foundation/Foundation.h>
 
+typedef enum : NSInteger {
+    PPStatusBarNotificationAnimationSlideToRight,
+    PPStatusBarNotificationAnimationSlideToLeft,
+    PPStatusBarNotificationAnimationSlideUp,
+    PPStatusBarNotificationAnimationSlideDown,
+} PPStatusBarNotificationAnimation;
+
 @interface PPStatusBarNotification : NSObject
 
 + (id)sharedNotification;
 - (void)showWithText:(NSString *)text;
+
+- (void)displayText:(NSString *)text withAnimation:(PPStatusBarNotificationAnimation)animation;
 
 @end
