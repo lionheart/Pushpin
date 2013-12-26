7a148a40a4ac89aff3e758a67d863a8356669cb6
Dan Loewenherz <dloewenherz@gmail.com>
2013-12-25 22:46:17 -0600

    add screenshotviewcontroller and ppstatusbarnotification

---
+++ Pinboard for iPhone/ScreenshotViewController.h
@@ -0,0 +1,13 @@
+//
+//  ScreenshotViewController.h
+//  Pushpin
+//
+//  Created by Dan Loewenherz on 12/25/13.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ScreenshotViewController : UIViewController
+
+@end
