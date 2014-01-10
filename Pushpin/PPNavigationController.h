9868740080530d29cf66e51e3f419cd9397a9bd1
Dan Loewenherz <dloewenherz@gmail.com>
2014-01-10 11:55:08 -0600

    minor iPad fixes with web view and switching orientation

--- Pushpin/PPNavigationController.h
+++ Pushpin/PPNavigationController.h
@@ -11,4 +11,6 @@
 
 @interface PPNavigationController : UINavigationController <UINavigationControllerDelegate, UIGestureRecognizerDelegate>
 
+@property (nonatomic, strong) UIBarButtonItem *splitViewControllerBarButtonItem;
+
 @end
