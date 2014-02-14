ace19b1542dd444f549c6e88f41c167440923d6e
Dan Loewenherz <dloewenherz@gmail.com>
2014-02-13 20:56:19 -0600

    style MFMailComposeController status bar style to be white

---
+++ Pushpin/MFMailComposeViewController+Theme.h
@@ -0,0 +1,13 @@
+//
+//  MFMailComposeViewController+Theme.h
+//  Pushpin
+//
+//  Created by Dan Loewenherz on 2/13/14.
+//
+//
+
+#import <MessageUI/MessageUI.h>
+
+@interface MFMailComposeViewController (Theme)
+
+@end
