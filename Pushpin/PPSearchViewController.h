ddc3877e5da626a33c303fa0fe452a0a151ff31b
Dan Loewenherz <dloewenherz@gmail.com>
2014-02-22 22:39:17 -0600

    improve search interface, let users search network, all, FTS, and more
    refs #388

--- Pushpin/PPSearchViewController.h
+++ Pushpin/PPSearchViewController.h
@@ -9,13 +9,34 @@
 #import "PPTableViewController.h"
 
 typedef enum : NSInteger {
+    PPSearchScopeMine,
+    
+#ifdef PINBOARD
+    PPSearchScopePinboard,
+#endif
+
+    PPSearchScopeNetwork,
+    PPSearchScopeEveryone,
+} PPSearchScopeType;
+
+static NSArray *PPSearchScopes() {
+//    return @[@"Mine", @"Pinboard Search", @"Network", @"Everyone"];
+    return @[@"Mine", @"Pinboard Search"];
+}
+
+typedef enum : NSInteger {
     PPSearchSectionQuery,
+    PPSearchSectionScope,
     PPSearchSectionFilters
 } PPSearchSectionType;
 
 typedef enum : NSInteger {
     PPSearchQueryRow
 } PPSearchQueryRowType;
+
+typedef enum : NSInteger {
+    PPSearchScopeRow
+} PPSearchScopeRowType;
 
 #ifdef PINBOARD
 typedef enum : NSInteger {
@@ -36,6 +57,7 @@ typedef enum : NSInteger {
 
 typedef enum : NSInteger {
     PPSearchQueryRowCount = PPSearchQueryRow + 1,
+    PPSearchScopeRowCount = PPSearchScopeRow + 1,
     PPSearchFilterRowCount = PPSearchFilterUntagged + 1
 } PPSearchRowCounts;
 
