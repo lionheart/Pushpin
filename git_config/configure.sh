a96345c4bd276c873c72424efec6db3fcb3e508d
Zixiao Wang <wangzixiao0330@gmail.com>
2014-06-26 15:20:21 -0500

    update configuration script

--- git_config/configure.sh
+++ git_config/configure.sh
@@ -8,6 +8,7 @@ git config merge.bundleversion.name "bundle version merge driver"
 git config merge.bundleversion.driver "git_config/bundle-merge-driver %O %A %B"
 
 # Install hooks
+mkdir -p .git/hooks
 cp git_config/hooks/post-checkout .git/hooks
 
 if [ -f git_config/hooks/post-checkout-user ]; then
