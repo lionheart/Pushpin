5e96d81ededb0ce4a40dde322e7c1f310813af48
Dan Loewenherz <dloewenherz@gmail.com>
2014-08-05 10:42:05 -0500

    add fades

---
+++ Pushpin/PPFadeTransition.m
@@ -0,0 +1,24 @@
+//
+//  PPFadeTransition.m
+//  Pushpin
+//
+//  Created by Dan Loewenherz on 8/5/14.
+//  Copyright (c) 2014 Lionheart Software. All rights reserved.
+//
+
+#import "PPFadeTransition.h"
+#import "PPFadeAnimator.h"
+
+@implementation PPFadeTransition
+
+- (id<UIViewControllerAnimatedTransitioning>)animationControllerForDismissedController:(UIViewController *)dismissed {
+    PPFadeAnimator *animator = [PPFadeAnimator new];
+    animator.reverse = YES;
+    return animator;
+}
+
+- (id<UIViewControllerAnimatedTransitioning>)animationControllerForPresentedController:(UIViewController *)presented presentingController:(UIViewController *)presenting sourceController:(UIViewController *)source {
+    return [PPFadeAnimator new];
+}
+
+@end
