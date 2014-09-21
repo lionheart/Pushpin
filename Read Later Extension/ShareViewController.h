f7bfb61f1ad305e9c385932675d9032bcb98c9e8
Dan Loewenherz <dloewenherz@gmail.com>
2014-09-21 13:44:37 -0500

    add read later extension

---
+++ Read Later Extension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  Read Later Extension
+//
+//  Created by Dan Loewenherz on 9/21/14.
+//  Copyright (c) 2014 Lionheart Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : SLComposeServiceViewController
+
+@end
