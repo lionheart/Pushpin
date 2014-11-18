ba2d4fe2aa5ff8550a97fc7f661768d794d12b3c
Dan Loewenherz <dloewenherz@gmail.com>
2014-11-18 15:41:47 -0600

    add PPOfflineSettingsViewController

---
+++ Pushpin/PPOfflineSettingsViewController.m
@@ -0,0 +1,17 @@
+//
+//  PPOfflineSettingsViewController.m
+//  Pushpin
+//
+//  Created by Dan Loewenherz on 11/4/14.
+//  Copyright (c) 2014 Lionheart Software. All rights reserved.
+//
+
+#import "PPOfflineSettingsViewController.h"
+
+@interface PPOfflineSettingsViewController ()
+
+@end
+
+@implementation PPOfflineSettingsViewController
+
+@end
