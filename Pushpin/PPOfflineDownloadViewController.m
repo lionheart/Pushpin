4661bb854b7c5254b5ec48fec5c7940c9c4fac27
Dan Loewenherz <dloewenherz@gmail.com>
2015-01-05 10:43:32 -0600

    commit for safekeeping

--- Pushpin/PPOfflineDownloadViewController.m
+++ Pushpin/PPOfflineDownloadViewController.m
@@ -10,6 +10,7 @@
 #import "PPURLCache.h"
 #import "PPAppDelegate.h"
 #import "PPTheme.h"
+#import "NSString+Additions.h"
 
 #import <LHSCategoryCollection/UIView+LHSAdditions.h>
 #import <LHSTableViewCells/LHSTableViewCellSubtitle.h>
@@ -24,6 +25,8 @@ static NSString *CellIdentifier = @"CellIdentifier";
 @property (nonatomic, strong) UILabel *assetLabel;
 @property (nonatomic, strong) UILabel *htmlLabel;
 
+- (void)dismissViewController;
+
 @end
 
 @implementation PPOfflineDownloadViewController
@@ -31,7 +34,18 @@ static NSString *CellIdentifier = @"CellIdentifier";
 - (void)viewDidLoad {
     [super viewDidLoad];
 
+    self.title = @"Downloading";
     self.view.backgroundColor = HEX(0xF7F9FDFF);
+    self.edgesForExtendedLayout = UIRectEdgeNone;
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:@"Stop"
+                                                                              style:UIBarButtonItemStyleDone
+                                                                             target:self
+                                                                             action:@selector(dismissViewController)];
+    
+    UIActivityIndicatorView *activity = [[UIActivityIndicatorView alloc] initWithActivityIndicatorStyle:UIActivityIndicatorViewStyleWhite];
+    [activity startAnimating];
+
+    self.navigationItem.leftBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:activity];
 
     self.assetProgressView = [[UIProgressView alloc] init];
     self.assetProgressView.progress = 0;
@@ -41,13 +55,70 @@ static NSString *CellIdentifier = @"CellIdentifier";
     self.htmlProgressView.progress = 0;
     self.htmlProgressView.translatesAutoresizingMaskIntoConstraints = NO;
     
+    UILabel *assetName = [[UILabel alloc] init];
+    assetName.font = [PPTheme textLabelFont];
+    assetName.text = @"CSS, JS, & Images";
+    assetName.translatesAutoresizingMaskIntoConstraints = NO;
+
+    UILabel *htmlName = [[UILabel alloc] init];
+    htmlName.font = [PPTheme textLabelFont];
+    htmlName.text = @"HTML";
+    htmlName.translatesAutoresizingMaskIntoConstraints = NO;
+    
     self.assetLabel = [[UILabel alloc] init];
     self.assetLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    self.assetLabel.backgroundColor = [UIColor whiteColor];
+    self.assetLabel.font = [PPTheme detailLabelFontAlternate1];
+    self.assetLabel.text = @"0 / 0";
     
     self.htmlLabel = [[UILabel alloc] init];
     self.htmlLabel.translatesAutoresizingMaskIntoConstraints = NO;
+    self.htmlLabel.backgroundColor = [UIColor whiteColor];
+    self.htmlLabel.font = [PPTheme detailLabelFontAlternate1];
+    self.htmlLabel.text = @"0%";
     
-    [self.tableView registerClass:[LHSTableViewCellSubtitle class] forCellReuseIdentifier:CellIdentifier];
+    UIView *htmlContainer = [[UIView alloc] init];
+    htmlContainer.translatesAutoresizingMaskIntoConstraints = NO;
+    htmlContainer.backgroundColor = [UIColor whiteColor];
+    
+    UIView *assetContainer = [[UIView alloc] init];
+    assetContainer.translatesAutoresizingMaskIntoConstraints = NO;
+    assetContainer.backgroundColor = [UIColor whiteColor];
+
+    NSDictionary *views = @{@"assetProgress": self.assetProgressView,
+                            @"htmlProgress": self.htmlProgressView,
+                            @"asset": self.assetLabel,
+                            @"html": self.htmlLabel,
+                            @"assetName": assetName,
+                            @"htmlName": htmlName,
+                            @"htmlContainer": htmlContainer,
+                            @"assetContainer": assetContainer };
+
+    [htmlContainer addSubview:htmlName];
+    [htmlContainer addSubview:self.htmlLabel];
+    [htmlContainer addSubview:self.htmlProgressView];
+
+    [assetContainer addSubview:assetName];
+    [assetContainer addSubview:self.assetLabel];
+    [assetContainer addSubview:self.assetProgressView];
+
+    [assetContainer lhs_addConstraints:@"V:|-4-[assetName][asset]-3-[assetProgress(10)]|" views:views];
+    [assetContainer lhs_addConstraints:@"H:|-10-[asset]-10-|" views:views];
+    [assetContainer lhs_addConstraints:@"H:|-10-[assetName]-10-|" views:views];
+
+    [htmlContainer lhs_addConstraints:@"V:|-4-[htmlName][html]-3-[htmlProgress(10)]|" views:views];
+    [htmlContainer lhs_addConstraints:@"H:|-10-[html]-10-|" views:views];
+    [htmlContainer lhs_addConstraints:@"H:|-10-[htmlName]-10-|" views:views];
+
+    [self.assetProgressView lhs_fillWidthOfSuperview];
+    [self.htmlProgressView lhs_fillWidthOfSuperview];
+    
+    [self.view addSubview:assetContainer];
+    [self.view addSubview:htmlContainer];
+    
+    [self.view lhs_addConstraints:@"V:|-20-[htmlContainer]-20-[assetContainer]" views:views];
+    [assetContainer lhs_fillWidthOfSuperview];
+    [htmlContainer lhs_fillWidthOfSuperview];
 }
 
 - (void)viewDidAppear:(BOOL)animated {
@@ -56,80 +127,44 @@ static NSString *CellIdentifier = @"CellIdentifier";
     dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
         PPURLCache *cache = [PPAppDelegate sharedDelegate].urlCache;
         [cache initiateBackgroundDownloadsWithCompletion:^(NSInteger count) {
-        } progress:^(NSInteger htmlCurrent, NSInteger htmlTotal, NSInteger assetCurrent, NSInteger assetTotal) {
+        } progress:^(NSString *urlString, NSInteger htmlCurrent, NSInteger htmlTotal, NSInteger assetCurrent, NSInteger assetTotal) {
             dispatch_async(dispatch_get_main_queue(), ^{
+                static NSNumberFormatter *formatter;
+                static dispatch_once_t onceToken;
+                dispatch_once(&onceToken, ^{
+                    formatter = [[NSNumberFormatter alloc] init];
+                    formatter.numberStyle = NSNumberFormatterPercentStyle;
+                    formatter.minimumFractionDigits = 1;
+                });
+
                 float htmlProgress = (float)htmlCurrent / (float)htmlTotal;
-                
                 float assetProgress;
                 if (assetTotal > 0) {
                     assetProgress = (float)assetCurrent / (float)assetTotal;
+                    [self.assetProgressView setProgress:assetProgress animated:YES];
+                    self.assetLabel.text = [NSString stringWithFormat:@"%lu / %lu", (long)assetCurrent, (long)assetTotal];
                 }
                 else {
                     assetProgress = 0;
+                    [self.assetProgressView setProgress:assetProgress animated:NO];
+                    self.assetLabel.text = @"-";
                 }
 
-                [self.assetProgressView setProgress:assetProgress animated:YES];
+//                self.htmlLabel.text = [formatter stringFromNumber:@(htmlProgress)];
+                self.htmlLabel.text = [urlString originalURLString];
                 [self.htmlProgressView setProgress:htmlProgress animated:YES];
-                
-                NSDictionary *attributes = @{NSFontAttributeName: [PPTheme detailLabelFont]};
-                NSMutableAttributedString *htmlString = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"HTML\n%f", htmlProgress]
-                                                                                               attributes:attributes];
-                [htmlString addAttribute:NSFontAttributeName value:[PPTheme textLabelFont] range:NSMakeRange(0, 4)];
-
-                NSMutableAttributedString *assetString = [[NSMutableAttributedString alloc] initWithString:[NSString stringWithFormat:@"CSS, JS, & Images\n%f", assetProgress]
-                                                                                                attributes:attributes];
-                [assetString addAttribute:NSFontAttributeName value:[PPTheme textLabelFont] range:NSMakeRange(0, 17)];
-
-                self.htmlLabel.attributedText = htmlString;
-                self.assetLabel.attributedText = assetString;
             });
         }];
     });
 }
 
-#pragma mark - UITableViewDataSource
-
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
-    return 2;
+- (void)dismissViewController {
+    [[PPAppDelegate sharedDelegate].urlCache stopAllDownloads];
+    [self.parentViewController dismissViewControllerAnimated:YES completion:nil];
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return 2;
-}
-
-- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
-    return 44;
-}
-
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
-    cell.textLabel.font = [PPTheme textLabelFont];
-    cell.detailTextLabel.font = [PPTheme detailLabelFont];
-    [cell.contentView lhs_removeSubviews];
-    
-    UIProgressView *progressView;
-    UILabel *label;
-    if (indexPath.section == 0) {
-        progressView = self.htmlProgressView;
-        label = self.htmlLabel;
-    }
-    else {
-        progressView = self.assetProgressView;
-        label = self.assetLabel;
-    }
-
-    if (indexPath.row == 0) {
-        [cell.contentView addSubview:label];
-        [label lhs_fillWidthOfSuperview];
-        [label lhs_fillHeightOfSuperview];
-    }
-    else {
-        [cell.contentView addSubview:progressView];
-        [progressView lhs_fillHeightOfSuperview];
-        [progressView lhs_fillWidthOfSuperview];
-    }
-
-    return cell;
+- (UIStatusBarStyle)preferredStatusBarStyle {
+    return UIStatusBarStyleLightContent;
 }
 
 @end
