ad4a494305633d2934d19276f7fc71fe950d2884
Dan Loewenherz <dan@lionheartsw.com>
2016-07-09 14:37:36 -0500

    update pod target support files

--- Pods/Target Support Files/LHSDelicious/LHSDelicious-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LHSDelicious : NSObject
-@end
-@implementation PodsDummy_LHSDelicious
-@end
--- Pods/Target Support Files/LHSDiigo/LHSDiigo-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_LHSDiigo : NSObject
-@end
-@implementation PodsDummy_LHSDiigo
-@end
