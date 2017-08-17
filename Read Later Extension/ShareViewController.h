66ba4071475cb6ca5fcbb017f38f77d06301d0d5
Dan Loewenherz <dan@lionheartsw.com>
2017-08-17 12:38:39 -0500

    update

---
+++ Read Later Extension/ShareViewController.h
@@ -0,0 +1,14 @@
+//
+//  ShareViewController.h
+//  Read Later Extension
+//
+//  Created by Daniel Loewenherz on 8/17/17.
+//  Copyright © 2017 Lionheart Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <Social/Social.h>
+
+@interface ShareViewController : UIAlertController
+
+@end
---
+++ Read Later Extension/ShareViewController.m
@@ -0,0 +1,25 @@
+//
+//  ShareViewController.m
+//  Read Later Extension
+//
+//  Created by Daniel Loewenherz on 8/17/17.
+//  Copyright © 2017 Lionheart Software. All rights reserved.
+//
+
+#import "ShareViewController.h"
+
+@interface ShareViewController ()
+
+@end
+
+@implementation ShareViewController
+
+- (instancetype)init {
+    self = [super init];
+    if (self) {
+
+    }
+    return self;
+}
+
+@end
