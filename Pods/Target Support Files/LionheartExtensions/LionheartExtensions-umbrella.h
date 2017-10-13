498cdc322021fd45536da5771d38f80286af0700
Dan Loewenherz <dan@lionheartsw.com>
2017-10-12 19:05:41 -0500

    update pod target support files

---
+++ Pods/Target Support Files/LionheartExtensions/LionheartExtensions-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double LionheartExtensionsVersionNumber;
+FOUNDATION_EXPORT const unsigned char LionheartExtensionsVersionString[];
+
