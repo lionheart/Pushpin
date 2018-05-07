4829b82832a5a429ce31e4a0ec1e779fd331e7ed
Dan Loewenherz <dan@lionheartsw.com>
2018-05-06 19:47:41 -0500

    update pod target support files

--- Pods/Target Support Files/Mixpanel-AppExtension/Mixpanel-AppExtension-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Mixpanel_AppExtension : NSObject
-@end
-@implementation PodsDummy_Mixpanel_AppExtension
-@end
--- Pods/Target Support Files/TipJarViewController/TipJarViewController-umbrella.h
+++ Pods/Target Support Files/TipJarViewController/TipJarViewController-umbrella.h
@@ -10,9 +10,7 @@
 #endif
 #endif
 
-#import "Mixpanel.h"
-#import "MixpanelPeople.h"
 
-FOUNDATION_EXPORT double Mixpanel_AppExtensionVersionNumber;
-FOUNDATION_EXPORT const unsigned char Mixpanel_AppExtensionVersionString[];
+FOUNDATION_EXPORT double TipJarViewControllerVersionNumber;
+FOUNDATION_EXPORT const unsigned char TipJarViewControllerVersionString[];
 
