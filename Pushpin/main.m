823a2e7e3d123f851f35a7a89333e40174ba856a
Dan Loewenherz <dan@lionheartsw.com>
2020-09-26 12:03:21 -0500

    update name

--- Pushpin/main.m
+++ Pushpin/main.m
@@ -3,7 +3,7 @@
 //  Pushpin
 //
 //  Created by Dan Loewenherz on 5/28/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Lionheart Software LLC. All rights reserved.
 //
 
 @import UIKit;
