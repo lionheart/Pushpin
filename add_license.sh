c7b07ee1c5809ce2515ac653da1b2c51346dcd3d
Dan Loewenherz <dan@lionheartsw.com>
2025-02-16 12:48:30 -0600

    update add license script

--- add_license.sh
+++ add_license.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
-addlicense -l none -f NOTICE Pushpin/*.m
+addlicense -l none -f NOTICE Pushpin/*.{m,h,swift}
+addlicense -l none -f NOTICE Bookmark\ Extension/*.{m,h,swift}
+addlicense -l none -f NOTICE Read\ Later\ Extension/*.{m,h}
